b72ec6b5247738376c85d0172821d75b9d1e46a9
mankec <milan.kecman270@gmail.com>
2022-11-07 20:01:02 +0100

    ngrok config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Config for ngrok
+  config.hosts << /[a-z0-9-.]+\.ngrok\.io/
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
