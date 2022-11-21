c6d1a8870e811adab44af48054c7e8699279dcd5
mankec <milan.kecman270@gmail.com>
2022-11-21 19:29:25 +0100

    Updated Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,26 @@
 class PagesController < ApplicationController
 
+  NUMBER_OF_SLIDES = 3
+
   def index
-    @pages = Page.all
+    @pages = Page.all.limit(5)
   end
 
   def show
-    @pages = Page.all
-    @page = Page.find(params[:id])
+    @pages = Page.all.limit(5)
+    @value = params[:id]
   end
 
-  def page
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.append('elements', partial: 'page', locals: { page: params[:id] })
-      end
-    end
+  def append
+    render partial: "member"
   end
+
+  # def page
+  #   respond_to do |format|
+  #     format.turbo_stream do
+  #       render turbo_stream: turbo_stream.append('swiper-slide', partial: 'page', locals: { page: params[:page].to_i })
+  #     end
+  #   end
+  # end
+
 end
