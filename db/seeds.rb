14efde260d7338140bac836205132b04f4867c77
mankec <milan.kecman270@gmail.com>
2022-11-21 19:27:02 +0100

    Added seeds for Page model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Page.destroy_all
+
+20.times do |i|
+  Page.create!(title: "Title #{i + 1}")
+end
