6581328208611ffbb026c3fe7c7bcbc228eb165b
mankec <milan.kecman270@gmail.com>
2022-11-29 12:01:39 +0100

    Updated javascript

--- app/javascript/swiper_initialize.js
+++ app/javascript/swiper_initialize.js
@@ -11,6 +11,10 @@ window.addEventListener('turbo:load', (event) => {
 function initializeSwipe(index) {
   console.log(`initializeSwipe ${index}`)
 
+  //
+  //TODO: Make page id in url to always be the same as the data-history attribute
+  //
+
   const swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,
     initialSlide: index,
@@ -18,30 +22,44 @@ function initializeSwipe(index) {
       el: ".swiper-pagination",
       clickable: true,
     },
+    history: {
+      key: 'pages',
+      replaceState: true,
+    },
   });
 
-  function appendSlide() {
+  let activeSlide = document.querySelector('.swiper-slide-active')
+  let pageId = activeSlide.getAttribute('data-history')
+
+  function appendSlide(id) {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
       swiper.appendSlide(this.responseText)
     }
-    xhttp.open("GET", `append`);
+    xhttp.open("GET", `../append/${id}`);
     xhttp.send();
   }
 
   swiper.on('slideChangeTransitionEnd', function () {
 
+    let index = swiper.activeIndex
     let lastSlideIndex = swiper.slides.length - 1
-    console.log(index)
-    console.log(lastSlideIndex)
-
 
     if (swiper.activeIndex == lastSlideIndex) {
       console.log('APPEND SLIDE')
-      appendSlide()
+      appendSlide(pageId)
+
+    } else {
+      swiper.destroy(false, false)
+      setTimeout(() => {
+        initializeSwipe(index)
+      }, 0)
     }
 
+
   })
+
+
 }
 
 
