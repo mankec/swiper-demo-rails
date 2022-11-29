91cc6288f9fbf932acba094f097ff93329a16741
mankec <milan.kecman270@gmail.com>
2022-11-29 12:31:17 +0100

    Updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages/25#show"
+  root to: "pages/25", controller: "pages", action: :show
   get "pages/:id", controller: "pages", action: :show
   get "append/:id", controller: "pages", action: :append
+
 end
